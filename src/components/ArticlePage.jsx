2694ec80b4f1258cce9e119804e6fe91345ff9c7
xiamonder <acldiamond@gmail.com>
2024-03-12 13:48:30 +0000

    basic single article page with related articles completed

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,7 +1,24 @@
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {useParams } from "react-router-dom";
+import { getArticleById } from "../articles_api_utils";
+import { FullArticleCard } from "./FullArticleCard";
+import { RelatedArticles } from "./RelatedArticles";
 
 export const ArticlePage = () => {
-const {pathname} = useLocation()
-  return <>
-  <h2>{pathname}</h2></>;
-};
+const {articleId} = useParams()
+console.log(articleId)
+const [article, setArticle] = useState('')
+
+
+useEffect(()=> {
+  getArticleById(articleId).then(({article})=> {
+    setArticle(article)
+  })
+}, [articleId])
+  return (
+<div>
+  <FullArticleCard article = {article}/>
+  <RelatedArticles article = {article} articleId ={articleId}/>
+</div>
+    )
+  };
